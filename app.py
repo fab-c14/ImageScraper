f87f9ac017f0896ef11bd8df8e0817d49243af3f
Faisal Bhat <plesim18@gmail.com>
2023-12-17 09:14:38 +0000

    Made some changes, moved to flask

--- app.py
+++ app.py
@@ -2,36 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import logging 
-# import pymongo
+from flask import Flask
+
 from pymongo.mongo_client import MongoClient
 import os
 
-
+app = Flask(__name__)
 save_dir = "images/"
-
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
-
-
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
-query = "pm narendra modi"
-response = requests.get(f"https://www.google.com/search?q={query}&sca_esv=587928711&rlz=1C1CHBF_enIN1048IN1048&tbm=isch&source=lnms&sa=X&ved=2ahUKEwit3pSZ0_eCAxVDyTgGHanECwMQ_AUoAnoECAMQBA&biw=798&bih=737&dpr=1")
+
+@app.route('/')
+def makingHomePage():
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
 
-soup = BeautifulSoup(response.content,'html.parser')
+    query = "pm narendra modi"
+    response = requests.get(f"https://www.google.com/search?q={query}&sca_esv=587928711&rlz=1C1CHBF_enIN1048IN1048&tbm=isch&source=lnms&sa=X&ved=2ahUKEwit3pSZ0_eCAxVDyTgGHanECwMQ_AUoAnoECAMQBA&biw=798&bih=737&dpr=1")
 
-image_tags = soup.find_all('img')
 
-len(image_tags)
+    soup = BeautifulSoup(response.content,'html.parser')
 
-del image_tags[0]
+    image_tags = soup.find_all('img')
 
-img_data_mongo = []
-for i in image_tags:
-    image_url = i['src']
-    image_data = requests.get(image_url).content
-    mydict = {
-        "index":image_url,"image":image_data
-    }
-    img_data_mongo.append(mydict)
+    len(image_tags)
+
+    del image_tags[0]
+
+    img_data_mongo = []
+    for i in image_tags:
+        image_url = i['src']
+        image_data = requests.get(image_url).content
+        mydict = {
+            "index":image_url,"image":image_data
+        }
+        img_data_mongo.append(mydict)
